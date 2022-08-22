adf4e91e74760cd3e29d5ed3457a70a078b60a22
programist <zhanybaevttt@gmail.com>
2022-08-22 17:38:47 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println(sqrt().apply(24.0));
+    }
+    public static UnaryOperator<Double>sqrt() {
+        UnaryOperator<Double>unaryOperator=(s) -> (Math.sqrt(s));
+        return unaryOperator;
     }
 }
